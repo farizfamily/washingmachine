65dffa52b66d44501648715643d4676068079fee
unknown <Ridei@ICT-RIDEI.bintang.com>
2025-07-20 01:26:57 +0700

    the significant different in this version we add bluetooth comm and user has some option washing mode

--- mesincuci_zc_v2b.ino
+++ mesincuci_zc_v2b.ino
@@ -10,22 +10,25 @@
 #define CCW 0
 dimmerLamp dimmer(outputPin); 
 int ke=0;
-int isi=6;
-int aduk=5;
-int nyuci=50;//i change from 70 to 50 on 20250710
+char data = 0;
 int kecepatan=0;
 int analogPin = 0;
 int istirahat=0;
 int highestspeed;
 int tenaga=25;
 int sambildrain=0;
-int keepatancuci=47;
-int lamaspin=32000;//30 menit
 int kecepatansense=70;
 int myInts[30];
 unsigned int  jml=0;
 int jmlrata=0;
+int isi=6;
+int aduk=5;
+int nyuci=55;//i change from 70 to 50 on 20250710
 unsigned int lamaistirahat=60000;
+unsigned int lamaistirahatantararah=5000;
+int keepatancuci=47;
+int lamaspin=32000;//30 menit
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -47,6 +50,19 @@ void loop() {
 
   //TELL USER WE WANT TO START
   for (int i = 0; i <= 10; i++) {
+  data=Serial.read();
+  Serial.println("pilih!1sd2");
+  Serial.println("1. Delicate");
+  Serial.println("2. Quick");
+  Serial.println("3. Heavy");
+
+  if(data=='1'){//gentle
+    Serial.println("under construcition");
+  }
+  if(data=='2'){
+    Serial.println("quick mode selected");
+    Serial.println("agitate 25x");
+  }
     digitalWrite(LED_BUILTIN, HIGH);
     delay(500);
     digitalWrite(LED_BUILTIN, LOW);
@@ -76,12 +92,12 @@ for(int lup1=0; lup1<=29; lup1++){
             delay(100);
             dimmer.setPower(slowdown);
           }
-			    break;
-		    }
+          break;
+        }
         delay(100);//500*20=10detik
-	      if(tenaga>49) tenaga=49;
-	      if(tenaga<25) tenaga=25;
-    	  dimmer.setPower(tenaga); // name.setPower(0%-100%)	   
+        if(tenaga>49) tenaga=49;
+        if(tenaga<25) tenaga=25;
+        dimmer.setPower(tenaga); // name.setPower(0%-100%)     
   }
   //dimmer.setPower(0); // name.setPower(0%-100%)
   dimmer.setState(OFF);
@@ -131,13 +147,13 @@ Serial.print("\n");
 
    //start water inTAKE
   for(int i = 0; i <= isi; i++) {  
-  	digitalWrite(INLET, LOW);
-  	delay(60000);// 1 menit
+    digitalWrite(INLET, LOW);
+    delay(60000);// 1 menit
     Serial.print("menit air ");
     Serial.print(i);
     Serial.print("\n");
-  	digitalWrite(INLET, HIGH);//istirahat biar gak panas selenoid
-  	delay(1000);
+    digitalWrite(INLET, HIGH);//istirahat biar gak panas selenoid
+    delay(1000);
   }
 
 
@@ -158,9 +174,9 @@ Serial.print("\n");
         if(kecepatan < keepatancuci) tenaga++;
         if(kecepatan > keepatancuci) tenaga--;
         delay(20);//500*20=10detik
-	      if(tenaga>49) tenaga=49;
-	      if(tenaga<25) tenaga=25;
-    	  dimmer.setPower(tenaga); // name.setPower(0%-100%)	   
+        if(tenaga>49) tenaga=49;
+        if(tenaga<25) tenaga=25;
+        dimmer.setPower(tenaga); // name.setPower(0%-100%)     
     }
     dimmer.setPower(10);
     dimmer.setState(OFF);
@@ -168,7 +184,7 @@ Serial.print("\n");
 
     //ganti arah
     digitalWrite(WASH_MOTOR_DIRECTION,CCW);
-    delay(1000 );				//THIS TO MAKE RELAY DIRECTION CONTROL LAST LONG
+    delay(1000 );       //THIS TO MAKE RELAY DIRECTION CONTROL LAST LONG
     tenaga=25; 
     dimmer.setPower(tenaga); // name.setPower(0%-100%)
     delay(100);
@@ -179,9 +195,9 @@ Serial.print("\n");
         if(kecepatan < keepatancuci) tenaga++;
         if(kecepatan > keepatancuci) tenaga--;
         delay(20);//500*20=10detik
-	      if(tenaga>49) tenaga=49;
-	      if(tenaga<25) tenaga=25;
-    	  dimmer.setPower(tenaga); // name.setPower(0%-100%)	   
+        if(tenaga>49) tenaga=49;
+        if(tenaga<25) tenaga=25;
+        dimmer.setPower(tenaga); // name.setPower(0%-100%)     
     }
     dimmer.setPower(10);
     dimmer.setState(OFF);
@@ -191,14 +207,14 @@ Serial.print("\n");
     Serial.print(i);
     Serial.print("\n");
     //add istirahat 
-	istirahat++;
+  istirahat++;
     if(istirahat==15) {
-		//lamaistirahat2 5=5mins;
-		for(int lamaistirahat2=0; lamaistirahat2<=6; lamaistirahat2++){
-			delay(lamaistirahat);//1 min
-		}
-	  istirahat=0;
-    }	
+    //lamaistirahat2 5=5mins;
+    for(int lamaistirahat2=0; lamaistirahat2<=6; lamaistirahat2++){
+      delay(lamaistirahat);//1 min
+    }
+    istirahat=0;
+    } 
   }
   //END WASH 
   //step 1 32.2detik*35 = 18.7menit, 
@@ -227,9 +243,9 @@ Serial.print("\n");
         if(kecepatan < keepatancuci) tenaga++;
         if(kecepatan > keepatancuci) tenaga--;
         delay(20);//1000*20=20detik
-	      if(tenaga>49) tenaga=49;
-	      if(tenaga<25) tenaga=25;
-    	  dimmer.setPower(tenaga); // name.setPower(0%-100%)	   
+        if(tenaga>49) tenaga=49;
+        if(tenaga<25) tenaga=25;
+        dimmer.setPower(tenaga); // name.setPower(0%-100%)     
     }
     dimmer.setPower(10);
     dimmer.setState(OFF);
@@ -242,14 +258,14 @@ Serial.print("\n");
 
   //DRAin
   for(int i = 0; i <= 2; i++) {  
-  	digitalWrite(DRAIN, LOW);
-  	delay(60000);
-  	digitalWrite(DRAIN, HIGH);//istirahat biar gak panas selenoid
-  	delay(10000);
+    digitalWrite(DRAIN, LOW);
+    delay(60000);
+    digitalWrite(DRAIN, HIGH);//istirahat biar gak panas selenoid
+    delay(10000);
   }
 
 
-	//SPIN
+  //SPIN
   digitalWrite(WASH_MOTOR_DIRECTION,CW);
   delay(1000 );//THIS TO MAKE RELAY DIRECTION CONTROL LAST LONG
   tenaga=25; 
@@ -266,13 +282,13 @@ Serial.print("\n");
         delay(10);
         if(tenaga>51) tenaga=51;
         if(tenaga<25) tenaga=25;
-        dimmer.setPower(tenaga); // name.setPower(0%-100%)	   
+        dimmer.setPower(tenaga); // name.setPower(0%-100%)     
     }
   }
    
   if(ke==1) lamaspin=30000;;
   if(ke==2) lamaspin=30000;;
-	for(int genjot = 0; genjot <= lamaspin; genjot++) {
+  for(int genjot = 0; genjot <= lamaspin; genjot++) {
     kecepatan = analogRead(analogPin);
     if(kecepatan < spinstep[9]) tenaga++;
     if(kecepatan > spinstep[9]) tenaga--;
@@ -287,22 +303,22 @@ Serial.print("\n");
     if(sambildrain==450){
       digitalWrite(DRAIN, HIGH);
       sambildrain=0;
-    }		  
+    }     
   }
   dimmer.setPower(10);
   dimmer.setState(OFF);
-	digitalWrite(DRAIN, HIGH);
+  digitalWrite(DRAIN, HIGH);
   delay(10000);
   //END SPIN
 
   if(ke==3){
-		digitalWrite(LED_BUILTIN, LOW);
-		while(true){
-			digitalWrite(LED_BUILTIN, HIGH);
-			delay(500);
-			digitalWrite(LED_BUILTIN, LOW);
-			delay(1000);
-		}
+    digitalWrite(LED_BUILTIN, LOW);
+    while(true){
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(500);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(1000);
+    }
   }
 }
       
